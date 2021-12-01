e31c38e2461cd14f0551a9c63fa0e77787922f42
Jimmy <me@jimmymorales.dev>
2021-12-01 14:14:13 -0600

    feat: Day 1 solution 🤓

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input.countMeasurementIncrease(windowSize = 1)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = input.countMeasurementIncrease(windowSize = 3)
 
-    // test if implementation meets criteria from the description, like:
+    // test if implementation meets criteria from the description, like what?:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+fun List<String>.countMeasurementIncrease(windowSize: Int) = windowed(windowSize) { it.sumOf(String::toInt) }
+    .zipWithNext()
+    .count { it.second > it.first }
